96b79205c0623061b2cc201344e12b8486446970
Abdulrahman Alattas <rmnattas@gmail.com>
2020-01-11 16:55:37 -0700

    api

--- HelloWorldWebsite/home/views.py
+++ HelloWorldWebsite/home/views.py
@@ -6,6 +6,9 @@ from django.template import loader
 from django.http import HttpResponse
 from django.views import generic
 from .models import Counter
+from opensky_api import OpenSkyApi
+import json
+import geopy.distance
 
 # Create your views here.
 class Home(generic.DetailView):
@@ -13,11 +16,40 @@ class Home(generic.DetailView):
     template_name = "home/index.html"
 
     def get(self, request, *args, **kwargs):
-        context = {'our_counter' : Counter.objects.get(pk=1)}
-        return render(request, self.template_name, context)
+        lat = float(request.GET["lat"])
+        lon = float(request.GET["lon"])
+        json = getPlanes(lat, lon)
+
+        return HttpResponse(json, content_type='application/json')
+
+        # context = {'our_counter' : Counter.objects.get(pk=1)}
+        # return render(request, self.template_name, context)
 
     def post(self, request, *args, **kwargs):
         counter_object = Counter.objects.get(pk=1)
         counter_object.count += 1
         counter_object.save()
         return redirect('homepage')
+
+
+
+def getPlanes(latitude, longitude):
+    bbox = [latitude - 5, latitude + 5, longitude - 5, longitude + 5]
+    s = OpenSkyApi().get_states(bbox=bbox)
+
+    result = []
+    for plane in s.states:
+    	result.append({
+    	"callsign": plane.callsign.strip(),
+    	"latitude": plane.latitude,
+    	"longitude": plane.longitude,
+    	"heading": plane.heading,
+    	"distance_km": geopy.distance.geodesic((latitude, longitude), (plane.latitude, plane.longitude)).km
+    	})
+
+    result.sort(key=lambda x: x["distance_km"])
+    dump = json.dumps(result[:3])
+    return(dump)
+
+
+
